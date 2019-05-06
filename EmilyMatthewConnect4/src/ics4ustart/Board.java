2c4afd3b3606f7f8118b6238d06b400d377d110a
324636513 <324636513@C-2V5ZJM1.tdsb.on.ca>
2019-05-06 12:46:41 -0400

    Can place a piece, and check verical and diagonal winner

--- EmilyMatthewConnect4/src/ics4ustart/Board.java
+++ EmilyMatthewConnect4/src/ics4ustart/Board.java
@@ -32,11 +32,80 @@ public class Board {
 	}
 	
 	
-	
+	public void piece(CellState player, int col) {
+		col = col -1;
+		for(int i=getRows()-1; i>=0; i--) {
+			if(board[i][col].getState()==CellState.EMPTY) {
+				board[i][col].setState(player);
+				return;
+			}
+			
+		}
+		
+	}
 	
 		
+	public boolean DiagonalWinner(int col, CellState player) {
+		int counter = 0;
+		//adjust col
+		col = col-1;
+		for(int i=0; i<getRows(); i++) {
+			for(int c=0; c<getCols(); c++) {
+				
+				if(board[i][c].getState()==player) {
+					counter++;
+				}
+				else {
+					counter=0;
+				}
+			}
+		}
+		return (counter==4);
+	}
 	
+	public boolean VerticalWinner(int col, CellState player) {
+		int counter = 0;
+		// adjust col
+		col = col-1;
+		
+		for(int i=0; i<getCols(); i++) {
+			
+				if(board[i][col].getState()==player) {
+					counter++; // found player at this location
+				}
+				else {
+					counter=0; // found different player or empty cell
+				}
+				
+			}
+		return (counter==4);
 	
+	}
+	public void HorizontalWinner(int col, int row) {
+		int counter1 = 0;
+		int counter2 = 0;
+		for(int i=0; i<getCols()-1; i++) {
+			for(int c=0; c<getRows()-1; c++) {
+				if(board[i][c].getState()==CellState.P1) {
+					counter1 +=1;
+				}
+				if(board[i][c].getState()==CellState.P2) {
+					counter2 +=1;
+				}
+				if(counter1 >=4) {
+					System.out.println("Player one Wins!");
+					return;
+				}
+				if(counter2 >=4) {
+					System.out.println("Player two Wins!");
+					return;
+				}
+			}
+		}
+		
+	}
+	
+
 	public void display() {
 		System.out.println("BOARD");
 		for (int i = 0; i < rows; i++) {
--- EmilyMatthewConnect4/src/ics4ustart/Driver.java
+++ EmilyMatthewConnect4/src/ics4ustart/Driver.java
@@ -1,4 +1,4 @@
-package ics4ustart;
+ package ics4ustart;
 
 import java.util.Scanner;
 import java.util.ArrayList;
@@ -6,10 +6,12 @@ import java.util.List;
 
 public class Driver {
 
+	private static final CellState[][] Cell = null;
+
 	public static void main(String[] args) throws InterruptedException {
 
 		// Setup constants for the Board
-		final int ROWS = 6; // change
+		final int ROWS = 7; // change
 		final int COLS = 7;
 
 		// create the board
@@ -21,9 +23,49 @@ public class Driver {
 
 		int column = 0;
 
+		
+		CellState currentPlayer = CellState.P1;
+		 
 		while (!done) {
-			column = getColumn();
-			board.display();
+			if(currentPlayer == CellState.P1) {
+				System.out.println("Player one turn");
+				column = getColumn();
+				board.piece(currentPlayer, column);
+				//Horizontal Winner
+				if(board.DiagonalWinner(column, currentPlayer)) {
+					System.out.println("Player " + currentPlayer + " is a winner!");
+					done=true;
+				}
+				//Vertical Winner
+				if (board.VerticalWinner(column,currentPlayer)) {
+					System.out.println("Player " + currentPlayer + " is a winner!");
+					done=true;
+				}
+				//Diagonal Winner
+				board.HorizontalWinner(column, ROWS);
+				board.display();
+				currentPlayer = CellState.P2;
+			}
+			else {
+				System.out.println("Player two turn");
+				column = getColumn();
+				board.piece(currentPlayer, column);
+				//Horizontal Winner
+				if(board.DiagonalWinner(column, currentPlayer)) {
+					System.out.println("Player " + currentPlayer + " is a winner!");
+					done=true;
+				}
+				//Vertical Winner;
+				if (board.VerticalWinner(column,currentPlayer)) {
+					System.out.println("Player " + currentPlayer + " is a winner!");
+					done=true;
+				}
+				// Diagonal Winner
+				board.HorizontalWinner(column, ROWS);
+				board.display();
+				currentPlayer = CellState.P1;
+			}
+			
 		}
 	}
 
@@ -32,7 +74,7 @@ public class Driver {
 		int column = 0;
 		Scanner in = new Scanner(System.in);
 
-		System.out.print("Which Column (1-7) :");
+		System.out.print("Which Column (1-7):");
 		column = Integer.parseInt(in.nextLine().trim());
 		return column;
 		
